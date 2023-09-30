8657b049b241ab1f4f5935b2f36c7851ea5ae03b
Azrael229 <israel.navarrete229@gmail.com>
2023-09-30 16:06:37 -0600

    cambios en meta etiquetas

--- index.php
+++ index.php
@@ -5,20 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <!-- SEO Meta Tags -->
-    <meta name="description" content="Your description">
-    <meta name="author" content="Your name">
+    <meta name="language" content="spanish">
+    <meta name="description" content="Gran experciencia en la certificación, normatividad y reparación de básculas, variedad de básculas para uso industrial y comercial">
+    <meta name="author" content="Servicom Basculas Digitales">
+    <meta name="keywords" content="básculas, calibración, reparación, industrial, Querétaro">
+    <meta name="robots" content="index, all, follow">
 
     <!-- OG Meta Tags to improve the way the post looks when you share the page on Facebook, Twitter, LinkedIn -->
-	<meta property="og:site_name" content="" /> <!-- website name -->
-	<meta property="og:site" content="" /> <!-- website link -->
-	<meta property="og:title" content=""/> <!-- title shown in the actual shared post -->
-	<meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
-	<meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
-	<meta property="og:url" content="" /> <!-- where do you want your post to link to -->
-	<meta name="twitter:card" content="summary_large_image"> <!-- to have large image post format in Twitter -->
+	<!-- <meta property="og:site_name" content="" /> website name -->
+	<!-- <meta property="og:site" content="" /> website link -->
+	<!-- <meta property="og:title" content=""/> title shown in the actual shared post -->
+	<!-- <meta property="og:description" content="" /> description shown in the actual shared post -->
+	<!-- <meta property="og:image" content="" /> image link, make sure it's jpg -->
+	<!-- <meta property="og:url" content="" /> where do you want your post to link to -->
+	<!-- <meta name="twitter:card" content="summary_large_image"> to have large image post format in Twitter -->
 
     <!-- Webpage Title -->
-    <title>Servicom Basculas Digitales</title>
+    <title>Basculas Digitales</title>
     
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
@@ -44,7 +47,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="h1-large">Basculas para uso industrial y comercial <br><br> Certificación y Planes de mantenimiento </h1>
+                        <h1 class="h1-large">Basculas para uso industrial y comercial <br><br> Certificación <br>Normatividad <br>Legalidad </h1>
                         <a class="btn-solid-lg" href="#contact">Cotización</a>
                     </div> <!-- end of col -->
                 </div> <!-- end of row -->
