2990e4b5e46823a38417bdf9e0b42c5a12960295
Azrael229 <israel.navarrete229@gmail.com>
2023-10-01 16:16:04 -0600

    csecion servicios introontenido y optimizacion SEO

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     
     <!-- SEO Meta Tags -->
     <meta name="language" content="spanish">
-    <meta name="description" content="Gran experciencia en la certificación, normatividad y reparación de básculas, variedad de básculas para uso industrial y comercial">
+    <meta name="description" content="Descubre nuestros servicios de reparación, calibración, verificación y venta de básculas industriales de la más alta calidad. Somos tu proveedor confiable en la industria, ofreciendo soluciones precisas y eficientes para mantener tus básculas en óptimas condiciones. ¡Contáctanos hoy para optimizar tu operación y garantizar la precisión en cada pesada!">
     <meta name="author" content="Servicom Basculas Digitales">
-    <meta name="keywords" content="básculas, calibración, reparación, industrial, Querétaro">
+    <meta name="keywords" content="Reparación de básculas digitales industriales en Querétaro">
     <meta name="robots" content="index, all, follow">
 
     <!-- OG Meta Tags to improve the way the post looks when you share the page on Facebook, Twitter, LinkedIn -->
@@ -21,7 +21,7 @@
 	<!-- <meta name="twitter:card" content="summary_large_image"> to have large image post format in Twitter -->
 
     <!-- Webpage Title -->
-    <title>Basculas Digitales</title>
+    <title>Básculas Industriales: Reparación, Calibración y Venta | SERVICOM Básculas Digitales</title>
     
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
@@ -47,7 +47,9 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="h1-large">Basculas para uso industrial y comercial <br><br> Certificación <br>Normatividad <br>Legalidad </h1>
+                        <!-- <h1 class="h1-large">Basculas para uso industrial y comercial <br><br> Certificación <br>Normatividad <br>Legalidad </h1> -->
+                        <h1 class="h1-large">Básculas para uso industrial y comercial, normatividad y servicios de manteniminteo</h1>
+
                         <a class="btn-solid-lg" href="#contact">Cotización</a>
                     </div> <!-- end of col -->
                 </div> <!-- end of row -->
@@ -65,6 +67,63 @@
 
 
     <!-- Introduction -->
+    <div id="introduction" class="cards-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="h2-heading">Nuestros servicios de Básculas Industriales</h2>
+                    <p class="p-heading">Ofrecemos una gama completa de servicios de básculas industriales en México. Desde reparación y calibración hasta la venta de equipos de alta calidad, somos tu proveedor confiable en la industria. Garantizamos precisión y eficiencia en cada pesada. ¡Optimiza tus operaciones con nuestros servicios especializados!</p>
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-icon">
+                            <span class="fas fa-certificate"></span>
+                        </div>
+                        <div class="card-body">
+                            <h4 class="card-title">Certificación</h4>
+                            <div class="card-text">Si tu negocio o empresa opera bajo estándares internacionales (ISO) o normas mexicanas (NOM o NMX), es fundamental cumplir con los requisitos que establecen. Entre ellos se encuentra la calibración de los instrumentos de medición utilizados en los procesos internos, la cual debe ser realizada por un proveedor externo confiable. ¡Contáctanos y descubre cómo podemos ayudarte a cumplir con estas normativas de manera efectiva</div>
+                        </div>
+                    </div>
+                    <!-- end of card -->
+
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-icon">
+                            <span class="fas fa-clipboard-check"</span>
+                        </div>
+                        <div class="card-body">
+                            <h4 class="card-title">Inspección (verificación)</h4>
+                            <div class="card-text">Si utilizas una báscula, independientemente de su capacidad, en transacciones comerciales o para calcular tarifas de productos o servicios, es un requisito legal realizar inspecciones periódicas (anteriormente llamadas verificaciones). Evita multas y acércate a nosotros, estamos aquí para ayudarte a cumplir con la normativa</div>
+                        </div>
+                    </div>
+                    <!-- end of card -->
+
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-icon">
+                            <span class="fas fa-wrench"></span>
+                        </div>
+                        <div class="card-body">
+                            <h4 class="card-title">Plan de Mantenimiento</h4>
+                                <div class="card-text">¿Cansado de preocuparte por las diferencias de peso en tus operaciones? En SERVICOM Básculas Digitales, te ofrecemos más que un servicio de mantenimiento. Te brindamos la tranquilidad de tener tus básculas bajo control en todo momento.
+
+                                Olvida las sorpresas desagradables y las mediciones incorrectas. Con nuestros planes de mantenimiento, garantizamos que tus básculas se mantengan en óptimas condiciones a lo largo del tiempo, ofreciendo mediciones confiables en cada uso.
+
+                                No dejes que el peso sea una incógnita. Mantén tus básculas confiables y precisas con SERVICOM Básculas Digitales. ¡Descubre la diferencia en la precisión y la tranquilidad hoy mismo</div>
+                        </div>
+                    </div>
+                    <!-- end of card -->
+
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of cards-1 -->
+
+
     <!-- end of introduction -->
    
 
